708b887e866f8d88cd9df497791c3076f5162601
Anirban Kar <anirban.k59@gmail.com>
2023-07-25 19:47:09 +0530

    used startswith() to ensure spelling errors do not affect the answer and fix aesthetics

--- quiz.py
+++ quiz.py
@@ -1,76 +1,78 @@
-print("Hello, Welcome to my Pop Quiz!")
-response1 = input("Would you like to play? Please type Y to confirm yes or N to quit game. ")
-if response1.title() != "Y":  
+print("Hello, Welcome to my Pop Quiz!\n")
+response1 = input("Would you like to play? Please type Y to confirm yes or N to quit game. \n")
+if response1.title() != "Y":
     quit()
 
-print("OK, let's play!!!")
+print("OK, let's play!!!\n")
+print("Kindly note you can Press Control + D anytime to exit the game\n")
+print("This is a lazy guy's quiz, so you could type the entire answer, sure.\nAlternatively you could also type the first two or more letters of your answer and it'll suffice.\n")
 score = 0
 
-answer1 = input("Which state's Koloriang is contending for wettest place on Earth? Arunachal Mizoram ")
-if answer1.title() == "Arunachal":
+answer1 = input("Which state's Koloriang is contending for wettest place on Earth? Arunachal Mizoram \n").title()
+if answer1.startswith("A"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer2 = input("Who is the said to be the father of economics? Adam Smith Milton Keynes ")
-if answer2.title() == "Adam Smith":
+answer2 = input("Who is the said to be the father of economics? Adam Smith Milton Keynes \n").title()
+if answer2.startswith("A"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer3 = input("Which is the latest company to enter the USD 1 trillion market valuation camp? Netflix Nvidia ")
-if answer3.title() == "Nvidia":
+answer3 = input("Which is the latest company to enter the USD 1 trillion market valuation camp? Netflix Nvidia \n").title()
+if answer3.startswith("Nv"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer4 = input("Which was the constituency of the last Khasi Chief Minister Meghalaya had? Shella Nongpoh  ")
-if answer4.title() == "Nongpoh":
+answer4 = input("Which was the constituency of the last Khasi Chief Minister Meghalaya had? Shella Nongpoh  \n").title()
+if answer4.startswith("N"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer5 = input("Where was Jesus crucified? Golgotha Jerusalem ")
-if answer5.title() == "Golgotha":
+answer5 = input("Where was Jesus crucified? Golgotha Jerusalem \n").title()
+if answer5.startswith("G"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer6 = input("Which Indian state is called 'God's own country'? Goa Kerala ")
-if answer6.title() == "Kerala":
+answer6 = input("Which Indian state is called 'God's own country'? Goa Kerala \n").title()
+if answer6.startswith("K"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer7 = input("What is ISRO's third lunar mission called? Chandrayaan Aditya L1  ")
-if answer7.lower() == "chandrayaan":
+answer7 = input("What is ISRO's third lunar mission called? Chandrayaan Aditya L1  \n").title()
+if answer7.startswith("C"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer8 = input("Kendrick Lamar won which grammy in 2023? Best Rap Song Record of the Year  ")
-if answer8.title() == "Best Rap Song":
+answer8 = input("Kendrick Lamar won which grammy in 2023? Best Rap Song Record of the Year  \n").title()
+if answer8.startswith("B"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer9 = input("Who is the Finance Minister of India? Nirmala Piyush  ")
-if answer9.title() == "Nirmala":
+answer9 = input("Who is the Finance Minister of India? Nirmala Piyush  \n").title()
+if answer9.startswith("N"):
     print("Correct!")
     score += 1
 else:
     print("Incorrect!!")
 
-answer10 = input("With which country is India currently negotiating border dispute settlement? Pakistan China ")
-if answer10.title() == "China":
+answer10 = input("With which country is India currently negotiating border dispute settlement? Pakistan China \n").title()
+if answer10.startswith("C"):
     print("Correct!")
     score += 1
 else:
@@ -79,7 +81,9 @@ else:
 print("Thank you for finishing the quiz!!!")
 print("Your score is " + str(score) + " out of 10" )
 print("You got " + str(score/10*100) +"%")
-if score > 7:
+if score == 10:
+	print("You're a genius. Sit for Civils!")
+elif score > 7:
     print("Wow, well done on this quiz!")
 elif 4<= score <= 7:
     print("Good performace!")
